16bc80b0f81a1f8ba31c0d540b10307f708a4329
Dukhishyam Tudu <91971083+Dukhishyam025@users.noreply.github.com>
2025-02-13 15:32:04 +0530

    Add files via upload

---
+++ 01_Spring_App/src/main/java/in/ashokit/beans/DieselEngine.java
@@ -0,0 +1,14 @@
+package in.ashokit.beans;
+
+public class DieselEngine {
+	
+	public DieselEngine() {
+	System.out.println("DieselEngine::Construtor");
+	}
+	
+	public void start()
+	{
+		System.out.println("DieselEngine started...");
+	}
+
+}
---
+++ 01_Spring_App/src/main/java/in/ashokit/beans/PetrolEngine.java
@@ -0,0 +1,13 @@
+package in.ashokit.beans;
+
+public class PetrolEngine {
+
+	public PetrolEngine()
+	{
+		System.out.println("PetrolEngine::COnstructor");
+	}
+	public void start()
+	{
+		System.out.println("PetrolEngine started...");
+	}
+}
